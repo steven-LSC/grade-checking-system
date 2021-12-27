cbb28ff70496e9707fdd976aee1d109753a84e0e
Steven Lin <steven123890@gmail.com>
2021-12-27 20:20:05 +0800

    新增影片連結

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,14 +9,25 @@
     <div class="group">
       <p class="title"><span class="text-red-500">組內</span>差分檢核</p>
       <p class="note">
-        在開始檢核前，如果有不明白的地方，請詳閱說明文件，謝謝。
+        如果有不明白的地方，可以參考<a
+          class="text-blue-900"
+          href="https://www.youtube.com/watch?v=UL38oR1aUVQ&feature=youtu.be"
+          target="_blank"
+          >組內差分檢核示範影片</a
+        >，謝謝。
       </p>
       <button @click="$router.push('in-group')" class="purple">前往</button>
     </div>
+
     <div class="group">
       <p class="title"><span class="text-red-500">組間</span>差分檢核</p>
       <p class="note">
-        在開始檢核前，如果有不明白的地方，請詳閱說明文件，謝謝。
+        如果有不明白的地方，可以參考<a
+          class="text-blue-900"
+          href="https://www.youtube.com/watch?v=8fA-ZFlAYPE"
+          target="_blank"
+          >組間差分檢核示範影片</a
+        >，謝謝。
       </p>
       <button @click="$router.push('between-groups')" class="purple">
         前往
